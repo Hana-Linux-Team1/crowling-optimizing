5d6c38d8d24c37b4b5d6285a9711326e74765455
mj.kim <alswo9853@gmail.com>
2024-06-13 19:11:34 +0900

    asdfasdfasdf

--- pubsubv2/p.py
+++ pubsubv2/p.py
@@ -2,9 +2,8 @@ import redis
 import json
 import time
 from multiprocessing import Process
+import argparse
 
-# 프로세스 수
-MAX_PROCESSES = 3
 
 def producer_task(urls):
     # Redis 서버에 연결
@@ -15,15 +14,16 @@ def producer_task(urls):
         r.lpush('carrot_tasks', task)
         print(f'Task added: {task}')
 
-def manage_consumers():
-    from consumer import consumer_task  # consumer.py에 있는 consumer_task 함수 가져오기
+
+def manage_consumers(max_processes):
+    from c import consumer_task  # consumer.py에 있는 consumer_task 함수 가져오기
 
     # 프로세스 관리 리스트
     processes = []
 
     while True:
-        # 프로세스가 MAX_PROCESSES보다 작으면 새로운 프로세스를 시작
-        if len(processes) < MAX_PROCESSES:
+        # 프로세스가 max_processes보다 작으면 새로운 프로세스를 시작
+        if len(processes) < max_processes:
             # 소비자 프로세스 시작
             p = Process(target=consumer_task)
             p.start()
@@ -32,7 +32,7 @@ def manage_consumers():
             processes.append(p)
             print(f'Started process {p.pid}')
         else:
-            # 프로세스가 MAX_PROCESSES보다 크면 종료된 프로세스를 제거
+            # 프로세스가 max_processes보다 크면 종료된 프로세스를 제거
             for p in processes:
                 # 프로세스가 종료되었으면 제거
                 if not p.is_alive():
@@ -45,19 +45,30 @@ def manage_consumers():
         p.join()
         print(f'Joined process {p.pid}')
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Carrot Market Scraping')
+    parser.add_argument('--max_processes', type=int, default=3,
+                        help='Maximum number of consumer processes to run')
+    parser.add_argument('--index', type=int, default=783227594,
+                        help='Starting index number for the articles')
+    parser.add_argument('--count', type=int, default=100,
+                        help='Number of articles to scrape')
+    args = parser.parse_args()
+
+    max_processes = args.max_processes
+    start_index = args.index
+    count = args.count
+
     # 당근 마켓 게시글 URL
     url = 'https://www.daangn.com/articles/'
-
-    # 당근 마켓 게시글 번호
-    index = 783227594
 
     # 크롤링할 URL 리스트 초기화
     urls_to_scrape = []
 
-    # 100개의 게시글 URL을 크롤링할 URL 리스트에 추가
-    for i in range(0, 100):
-        tmp_index = index - i
+    # 지정된 개수만큼 게시글 URL을 크롤링할 URL 리스트에 추가
+    for i in range(count):
+        tmp_index = start_index - i
         article_url = f'{url}{tmp_index}'
         urls_to_scrape.append(article_url)
 
@@ -65,4 +76,4 @@ if __name__ == '__main__':
     producer_task(urls_to_scrape)
 
     # 소비자 프로세스 관리 시작
-    manage_consumers()
+    manage_consumers(max_processes)
